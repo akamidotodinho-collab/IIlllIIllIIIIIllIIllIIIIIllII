99a95a9254ce3234c3831e8431f43cdd6ccad8f5
llIlllIIIIIlllIIIlllIIIlllIIIIllllI <akamidotodinho@gmail.com>
2025-08-15 08:36:50 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  clearScreen: false,
   server: {
-    host: '0.0.0.0',
-    port: 5000,
-    allowedHosts: 'all'
+    port: 1420,
+    strictPort: true,
   },
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@assets': '/src/assets'
-    }
-  },
+  envPrefix: ['VITE_', 'TAURI_'],
   build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
 })
